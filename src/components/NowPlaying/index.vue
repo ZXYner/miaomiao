4c6273c8bf827a2738f5740844807bf4e9a3b208
ZXYner <976078956@qq.com>
2020-06-25 15:32:42 +0800

    add getCity

--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -1,109 +1,75 @@
 <template>
-   <div class="movie_body">
-        <ul>
-            <li>
-                <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-                <div class="info_list">
-                    <h2>无名之辈</h2>
-                    <p>观众评 <span class="grade">9.2</span></p>
-                    <p>主演: 陈建斌,任素汐,潘斌龙</p>
-                    <p>今天55家影院放映607场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-                <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-                <div class="info_list">
-                    <h2>无名之辈</h2>
-                    <p>观众评 <span class="grade">9.2</span></p>
-                    <p>主演: 陈建斌,任素汐,潘斌龙</p>
-                    <p>今天55家影院放映607场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-                <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-                <div class="info_list">
-                    <h2>无名之辈</h2>
-                    <p>观众评 <span class="grade">9.2</span></p>
-                    <p>主演: 陈建斌,任素汐,潘斌龙</p>
-                    <p>今天55家影院放映607场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-                <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-                <div class="info_list">
-                    <h2>无名之辈</h2>
-                    <p>观众评 <span class="grade">9.2</span></p>
-                    <p>主演: 陈建斌,任素汐,潘斌龙</p>
-                    <p>今天55家影院放映607场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-                <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-        </ul>
+   <div class="movie_body" ref="movie_body">
+       <Loading v-if="isLoading"/>
+        <Scroller v-else :handleToScroll="handleToScroll" :handleToTouchEnd="handleToTouchEnd">
+            <ul>
+                <li v-if="pullDown.length">{{pullDown}}</li>
+                <li v-for="item in movieList" :key="item.id">
+                    <div class="pic_show" @tap="handleToDetail"><img :src="item.img | setWH('128.180')"></div>
+                    <div class="info_list">
+                        <h2>{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png" /></h2>
+                        <p>观众评 <span class="grade">{{item.sc}}</span></p>
+                        <p>主演： {{item.star}}</p>
+                        <p>今天55家影院放映607场</p>
+                    </div>
+                    <div class="btn_mall">
+                        购票
+                    </div>
+                </li>
+            </ul>
+        </Scroller>
 	</div>
 </template>
 
 <script>
+
 export default {
-    name:"NowPlaying"
+    name:"NowPlaying",
+    data(){
+       return{
+           movieList:[],
+           pullDown:'',
+           isLoading:true,
+           prevCityId:-1
+       } 
+    },
+    activated(){
+        let cityId=this.$store.state.city.id;
+        if(this.prevCityId===cityId) return;
+        this.isLoading=true;
+         this.axios.get('/api/movieOnInfoList?cityId='+cityId).then((res)=>{
+            if(res.data.msg==='ok'){
+                this.movieList=res.data.data.movieList;
+                this.isLoading=false;
+                this.prevCityId=cityId
+            }
+        })
+    },
+    methods:{
+        // 使用tap事件，点击触发，滑动时并不触发
+        handleToDetail(){
+            console.log(111)
+        },
+        handleToScroll(pos){
+            if(pos.y>30){
+                this.pullDown='正在更新中······'
+            }
+        },
+        handleToTouchEnd(pos){
+            if(pos.y>30){
+                this.axios.get('/api/movieOnInfoList?cityId=9').then((res)=>{
+                if(res.data.msg==='ok'){
+                    this.pullDown='更新成功'
+                    setTimeout(()=>{
+                        this.movieList=res.data.data.movieList;
+                        this.pullDown='';
+                        // console.log(11)
+                    },1000)
+                }
+                })
+            }
+        }
+    }
 }
 
 </script>
--- src/views/Movie/index.vue
+++ src/views/Movie/index.vue
@@ -4,7 +4,7 @@
         <div id="content">
             <div class="movie_menu">
                 <router-link tag="div" to="/movie/city" class="city_name">
-                    <span>大连</span><i class="iconfont icon-lower-triangle"></i>
+                    <span>{{$store.state.city.nm}}</span><i class="iconfont icon-lower-triangle"></i>
                 </router-link>
                 <div class="hot_swtich">
                     <router-link tag="div" to="/movie/nowplaying" class="hot_item">正在热映</router-link>
@@ -25,11 +25,35 @@
 <script>
 import Header from '@/components/Header';
 import TabBar from '@/components/TabBar';
+import {messageBox} from '@/components/JS'
 export default {
     name:'Movie',
     components:{
         Header,
         TabBar
+    },
+    mounted(){
+        setTimeout(()=>{
+            this.axios.get('api/getLocation').then(res=>{
+                var msg=res.data.msg;
+                var nm=res.data.data.nm;
+                var id=res.data.data.id;
+                if(nm===this.$store.state.city.nm) return
+                if(msg==='ok'){
+                    messageBox({
+                        title:定位,
+                        content:nm,
+                        cancle:'取消',
+                        ok:'切换定位',
+                        handleOk(){
+                            window.localStorage.setTtem('nowNM',nm);
+                            window.localStorage.setTtem('nowID',id);
+                            window.location.reload()
+                        }
+                    })
+                }
+            })
+        },2000)   
     }
 }
 </script>
